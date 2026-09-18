96fdfdf52dbb826446d8c964c59eaf618c78761c
WPExplorer <wpexplorer@gmail.com>
2026-09-18 13:02:44 -0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -10,5 +10,6 @@
 ?>
 
 <form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_html_e( 'Search &hellip;', 'pronto' ); ?>" />
+	<label for="s" class="screen-reader-text"><?php esc_html_e( 'Search for:', 'pronto' ); ?></label>
+	<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'pronto' ); ?>" />
 </form>
