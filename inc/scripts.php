fa6804f9d1cc1e85f90df49d744eda1af6e49415
WPExplorer <wpexplorer@gmail.com>
2026-09-18 12:59:34 -0700

    Local hosted Google font.

--- inc/scripts.php
+++ inc/scripts.php
@@ -9,20 +9,22 @@
  */
 
 function wpex_load_scripts() {
-	
+
+	$dir     = get_template_directory_uri();
+	$version = wp_get_theme( get_template() )->get( 'Version' );
+
 	// CSS
-	wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_style( 'raleway-google-font', 'http://fonts.googleapis.com/css?family=Raleway:400,300,500,600,700', 'style' );
-	wp_enqueue_style( 'font-awesome', get_template_directory_uri().'/css/font-awesome.min.css', true );
-	
+	wp_enqueue_style( 'raleway', $dir . '/css/raleway.css', array(), $version );
+	wp_enqueue_style( 'font-awesome', $dir . '/css/font-awesome.min.css', array(), '4.6.1' );
+	wp_enqueue_style( 'style', get_stylesheet_uri(), array( 'raleway', 'font-awesome' ), $version );
 
 	// JS
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-	
-	wp_enqueue_script( 'wpex-masonry', get_template_directory_uri().'/js/masonry.pkgd.min.js', array( 'jquery' ), '1.0', true );
-	wp_enqueue_script( 'wpex-global', get_template_directory_uri().'/js/global.js', array( 'jquery', 'wpex-masonry' ), '1.0', true );
-	
+
+	wp_enqueue_script( 'wpex-masonry', $dir . '/js/masonry.pkgd.min.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'wpex-global', $dir . '/js/global.js', array( 'jquery', 'wpex-masonry' ), '1.0', true );
+
 }
 add_action( 'wp_enqueue_scripts', 'wpex_load_scripts' );
